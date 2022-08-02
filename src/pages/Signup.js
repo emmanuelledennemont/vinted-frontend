5f4bedb840afb30cd155adfff168be4c31c6f299
Emmanuelle  Dennemont <emmanuelledennemont@macbook-pro-de-emmanuelle.home>
2022-08-02 17:12:30 +0200

    link home

--- src/pages/Signup.js
+++ src/pages/Signup.js
@@ -2,12 +2,15 @@ import { useState } from "react";
 import { Link } from "react-router-dom";
 import axios from "axios";
 import Cookies from "js-cookie";
+import { useNavigate } from "react-router-dom";
 
 const Signup = ({ token, setUser, setToken }) => {
   const [username, setUsername] = useState("");
   const [password, setPassword] = useState("");
   const [mail, setMail] = useState("");
   const [newsletter, setNewsletter] = useState(false);
+  const [isLoading, setIsLoading] = useState(false);
+  const navigate = useNavigate();
 
   const changeUsername = (event) => {
     const value = event.target.value;
@@ -34,6 +37,7 @@ const Signup = ({ token, setUser, setToken }) => {
           event.preventDefault();
           const fetchData = async () => {
             try {
+              setIsLoading(true);
               const response = await axios.post(
                 "https://lereacteur-vinted-api.herokuapp.com/user/signup",
                 {
@@ -52,9 +56,11 @@ const Signup = ({ token, setUser, setToken }) => {
               setToken(
                 Cookies.set("token", response.data.token, { expires: 7 })
               );
+              navigate("/");
             } catch (error) {
               console.log(error.response);
             }
+            setIsLoading(false);
           };
           fetchData();
         }}
@@ -79,15 +85,15 @@ const Signup = ({ token, setUser, setToken }) => {
         />
         <div className="newsletter">
           <div className="input-chek">
-          <input
-            id="checkbox"
-            type="checkbox"
-            checked={newsletter}
-            onChange={() => {
-              setNewsletter((prevState) => !prevState);
-            }}
-          />
-          <label htmlFor="checkbox">S'inscrire à la newsletter</label>
+            <input
+              id="checkbox"
+              type="checkbox"
+              checked={newsletter}
+              onChange={() => {
+                setNewsletter((prevState) => !prevState);
+              }}
+            />
+            <label htmlFor="checkbox">S'inscrire à la newsletter</label>
           </div>
           <p>
             En m'inscrivant je confirme avoir lu et accepté les Termes &
@@ -95,18 +101,26 @@ const Signup = ({ token, setUser, setToken }) => {
             avoir au moins 18 ans.
           </p>
         </div>
-        <button type="submit" className="submit-btn">
-          S'inscrire
-        </button>
+        {isLoading ? (
+          <p>Inscription en cours ...</p>
+        ) : (
+          <button type="submit" className="submit-btn">
+            S'inscrire
+          </button>
+        )}
       </form>
       <Link to="/login" className="link">
         Tu as déjà un compte ? Connecte-toi !
       </Link>
     </div>
   ) : (
+    <Link to="/" className="link-home">
     <div className="form">
-        <p>Votre compte a été créé !</p>
+      <p>Votre compte a été créé !</p>
+      <span>Redirigez vous vers la page d'accueil en cliquant ici</span>
     </div>
+  </Link>
+    
   );
 };
 
